58c394a3dd1b77df61df5d3f846f6338f727b720
Leonardo Wildt <leonardod.trimarchi@gmail.com>
2024-01-23 19:40:15 -0500

    added input for url

--- src/routes/chat/page.svelte
+++ src/routes/chat/page.svelte
@@ -1,7 +1,7 @@
 <script>
     let isChatboxOpen = true;
     let userInput = "";
-
+    let apiEndpoint = "";
     let messages = [];
     let chatbox;
 
@@ -46,104 +46,117 @@
         Click the button below to chat with the AWS Bedrock chatbot.
     </p>
 </div>
-<div
-    class="h-screen overflow-hidden flex items-center justify-center"
-    style="background: #edf2f7;"
->
-    <div class="fixed bottom-0 right-0 mb-4 mr-4">
-        <button
-            on:click={toggleChatbox}
-            id="open-chat"
-            class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 flex items-center"
+<div class="flex h-screen overflow-hidden" style="background: #edf2f7;">
+    <div class="api-input-container p-4">
+        <label
+            for="api-endpoint"
+            class="block text-sm font-medium text-gray-700 mb-2"
         >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="w-6 h-6 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                ></path>
-            </svg>
-            Chat with AWS Bedrock
-        </button>
+            API Endpoint URL
+        </label>
+        <input
+            id="api-endpoint"
+            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+            placeholder="https://example.com/api"
+            bind:value={apiEndpoint}
+        />
     </div>
-    <div
-        id="chat-container"
-        class:hidden={!isChatboxOpen}
-        class="hidden fixed bottom-16 right-4 w-96"
-    >
-        <div class="bg-white shadow-md rounded-lg max-w-lg w-full">
-            <div
-                class="p-4 border-b bg-blue-500 text-white rounded-t-lg flex justify-between items-center"
+    <div class="flex-1">
+        <div class="fixed bottom-0 right-0 mb-4 mr-4">
+            <button
+                on:click={toggleChatbox}
+                id="open-chat"
+                class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 flex items-center"
             >
-                <p class="text-lg font-semibold">Admin Bot</p>
-                <button
-                    on:click={toggleChatbox}
-                    id="close-chat"
-                    class="text-gray-300 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-6 h-6 mr-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                    ></path>
+                </svg>
+                Chat with AWS Bedrock
+            </button>
+        </div>
+        <div
+            id="chat-container"
+            class:hidden={!isChatboxOpen}
+            class="hidden fixed bottom-16 right-4 w-96"
+        >
+            <div class="bg-white shadow-md rounded-lg max-w-lg w-full">
+                <div
+                    class="p-4 border-b bg-blue-500 text-white rounded-t-lg flex justify-between items-center"
+                >
+                    <p class="text-lg font-semibold">Admin Bot</p>
+                    <button
+                        on:click={toggleChatbox}
+                        id="close-chat"
+                        class="text-gray-300 hover:text-gray-400 focus:outline-none focus:text-gray-400"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M6 18L18 6M6 6l12 12"
-                        ></path>
-                    </svg>
-                </button>
-            </div>
-            <div
-                bind:this={chatbox}
-                id="chatbox"
-                class="p-4 h-80 overflow-y-auto"
-            >
-                <!-- Chat messages will be displayed here -->
-                {#each messages as message}
-                    {#if message.sender === "user"}
-                        <div class="mb-2 text-right">
-                            <p
-                                class="bg-blue-500 text-white rounded-lg py-2 px-4 inline-block"
-                            >
-                                {message.text}
-                            </p>
-                        </div>
-                    {:else if message.sender === "bot"}
-                        <div class="mb-2">
-                            <p
-                                class="bg-gray-200 text-gray-700 rounded-lg py-2 px-4 inline-block"
-                            >
-                                {message.text}
-                            </p>
-                        </div>
-                    {/if}
-                {/each}
-            </div>
-            <div class="p-4 border-t flex">
-                <input
-                    bind:value={userInput}
-                    id="user-input"
-                    type="text"
-                    placeholder="Type a message"
-                    class="w-full px-3 py-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <button
-                    on:click={sendMessage}
-                    id="send-button"
-                    class="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-600 transition duration-300"
-                    >Send</button
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-6 h-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+                <div
+                    bind:this={chatbox}
+                    id="chatbox"
+                    class="p-4 h-80 overflow-y-auto"
                 >
+                    <!-- Chat messages will be displayed here -->
+                    {#each messages as message}
+                        {#if message.sender === "user"}
+                            <div class="mb-2 text-right">
+                                <p
+                                    class="bg-blue-500 text-white rounded-lg py-2 px-4 inline-block"
+                                >
+                                    {message.text}
+                                </p>
+                            </div>
+                        {:else if message.sender === "bot"}
+                            <div class="mb-2">
+                                <p
+                                    class="bg-gray-200 text-gray-700 rounded-lg py-2 px-4 inline-block"
+                                >
+                                    {message.text}
+                                </p>
+                            </div>
+                        {/if}
+                    {/each}
+                </div>
+                <div class="p-4 border-t flex">
+                    <input
+                        bind:value={userInput}
+                        id="user-input"
+                        type="text"
+                        placeholder="Type a message"
+                        class="w-full px-3 py-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <button
+                        on:click={sendMessage}
+                        id="send-button"
+                        class="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-600 transition duration-300"
+                        >Send</button
+                    >
+                </div>
             </div>
         </div>
     </div>
